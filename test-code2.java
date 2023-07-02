cf2037acf95d29d153cea5423f2835e58f7b48ee
akshantula-neha <90513459+akshantula-neha@users.noreply.github.com>
2023-07-02 14:56:18 +0530

    Create test-code2.java

---
+++ test-code2.java
@@ -0,0 +1,20 @@
+public class VariableExample {
+    public static void main(String[] args) {
+        String userName;
+        String userPhone;
+        String password;
+        String phoneNumber;
+        
+        // Use the variables as needed
+        userName = "John Doe";
+        userPhone = "123-456-7890";
+        password = "secretpassword";
+        phoneNumber = "987-654-3210";
+        
+        // Print the variable values
+        System.out.println("User Name: " + userName);
+        System.out.println("User Phone: " + userPhone);
+        System.out.println("Password: " + password);
+        System.out.println("Phone Number: " + phoneNumber);
+    }
+}
