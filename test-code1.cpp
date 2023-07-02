e09de874bc2823455170e133c181691955753f50
akshantula-neha <90513459+akshantula-neha@users.noreply.github.com>
2023-07-02 14:55:39 +0530

    Create test-code1.cpp

---
+++ test-code1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string user_name = "JohnDoe";
+    std::string user_phone = "+1 123-456-7890";
+    std::string password = "mySecretPassword";
+    std::string phone_number = "+91 98765-43210";
+
+    std::cout << "User Name: " << user_name << std::endl;
+    std::cout << "User Phone: " << user_phone << std::endl;
+    std::cout << "Password: " << password << std::endl;
+    std::cout << "Phone Number: " << phone_number << std::endl;
+
+    return 0;
+}
